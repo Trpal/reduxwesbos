0491cf776ccb2e8ec1b3bac221d7806650b0d047
Trpal <jonikorpelin@gmail.com>
2018-03-02 19:09:39 +0200

    comments hooked up

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,8 +1,23 @@
 import React from "react";
+import Photo from "./Photo";
+// import comments
+import Comments from "./Comments";
 
 const Single = React.createClass({
 	render() {
-		return <div className="single-photo">im the Single</div>;
+		// index of the post
+		const { postId } = this.props.params;
+		const i = this.props.posts.findIndex(post => post.code === postId);
+		const post = this.props.posts[i];
+		const postComments = this.props.comments[postId] || [];
+
+		// get us the post
+		return (
+			<div className="single-photo">
+				<Photo i={i} post={post} {...this.props} />
+				<Comments postComments={postComments} {...this.props} />
+			</div>
+		);
 	}
 });
 
